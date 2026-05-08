5df273156176b091b36b311b6211396a8b83e19e
apollochocolate <jangeunhee02s@google.com>
2026-05-08 13:32:43 +0900

    lasertest.py add

---
+++ test_eunhee/lasertest.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+LASER_PIN = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LASER_PIN, GPIO.OUT)
+
+try:
+    print("Laser ON")
+    GPIO.output(LASER_PIN, GPIO.HIGH)
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Stopped")
+finally:
+    GPIO.cleanup()
